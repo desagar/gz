60bdb30ce931cd2bfd0e6bee2017a9c0dc497082
Deva Sagar <deva.sagar@oracle.com>
2018-05-11 11:02:04 -0400

    curl with -u

--- load_es.sh
+++ load_es.sh
@@ -9,15 +9,17 @@ CURL_DELETE_INDEX_CMD='curl -X DELETE -H "Content-Type: application/x-ndjson"'
 CURL_POST_CMD='curl -X POST -H "Content-Type: application/x-ndjson"'
 if [ -n "$ES_USER" ] ; then
     echo "Credentials provided - adding auth header for ES"
-    CURL_POST_CMD="${CURL_POST_CMD} -H \"Authorization: $(echo $ES_USER:$ES_PASS | base64)\""
-    CURL_DELETE_INDEX_CMD="${CURL_DELETE_INDEX_CMD} -H \"Authorization: $(echo $ES_USER:$ES_PASS | base64)\""
+    CURL_POST_CMD="${CURL_POST_CMD} -u $ES_USER:$ES_PASS"
+    CURL_DELETE_INDEX_CMD="${CURL_DELETE_INDEX_CMD} -u $ES_USER:$ES_PASS"
 else
     echo "No Credentials provided - accessing ES anonymously"
 fi
 CURL_DELETE_INDEX_CMD="${CURL_DELETE_INDEX_CMD} ${ES_URL}/cities"
 CURL_POST_CMD="${CURL_POST_CMD} --data-binary @bulk-cities-es.txt ${ES_URL}/_bulk"
 echo "Deleting index if exists"
+echo $CURL_DELETE_INDEX_CMD
 eval $CURL_DELETE_INDEX_CMD
 echo
 echo "Loading Data"
+echo $CURL_POST_CMD
 eval $CURL_POST_CMD
